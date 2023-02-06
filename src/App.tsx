63aff391ec7c62a46916aeab6fce08d12a712102
용준현 <yongjunhyeon@yongjunhyeon-ui-noteubug.local>
2023-02-06 20:02:26 +0900

    feat: body 태그로 다크모드 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ProjectUploadModal } from '@/components';
 
 export default function App() {
   const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const colorMode = window.localStorage.getItem('color_mode') || 'dark';
+    window.document.body.classList.add(colorMode);
+  }, []);
+
   return (
-    <div id="App" className="App">
-      <button onClick={() => setModalOpen((prev) => !prev)}>프로젝트 모달 버튼</button>
-      {modalOpen && <ProjectUploadModal />}
+    <div id="App" className="App" style={{ height: 3000 }}>
+      <button
+        onClick={() => setModalOpen((prev) => !prev)}
+        style={{ position: 'relative', zIndex: 1000 }}
+      >
+        프로젝트 모달 버튼
+      </button>
+      <button
+        onClick={() => {
+          const colorMode = window.document.body.classList.item(0);
+          if (colorMode === 'dark') {
+            window.document.body.classList.replace('dark', 'light');
+            window.localStorage.setItem('color_mode', 'light');
+          }
+          if (colorMode === 'light') {
+            window.document.body.classList.replace('light', 'dark');
+            window.localStorage.setItem('color_mode', 'dark');
+          }
+        }}
+        style={{ position: 'relative', zIndex: 1000 }}
+      >
+        다크 모드 활성화
+      </button>
+
+      <ProjectUploadModal isOpen={modalOpen} />
     </div>
   );
 }
