654e4b329895b01a776aafc3c0e356c8e8e9fd8d
pione3r <dev2diver@gmail.com>
2023-02-09 16:25:22 +0900

    feat: 프로젝트 리스트 받아오는 mock api 코드 추가

--- src/api/mocks/projectListHandler/index.ts
+++ src/api/mocks/projectListHandler/index.ts
@@ -4,14 +4,25 @@ export const projectListHandlers = [
   rest.get('/project-list', async (req, res, ctx) => {
     await waitLikeServer(2000);
 
-    const projectListCursor = Number(req.url.searchParams.get('cursor')) || 0;
+    const pageNum = Number(req.url.searchParams.get('page')) || 1;
+
+    // if (pageNum === 5) {
+    //   return res(
+    //     ctx.status(200),
+    //     ctx.json({
+    //       data: [...projectList.slice(25, 30)],
+    //       // nextPage: pageNum + 1,
+    //       isLastPage: true,
+    //     }),
+    //   );
+    // }
 
     return res(
-      ctx.status(201),
+      ctx.status(200),
       ctx.json({
-        data: [...projectList.slice(projectListCursor, projectListCursor + 5)],
-        nextCursor: projectListCursor + 5,
-        isLastPage: projectListCursor >= projectList.length - 5 ? true : false,
+        data: [...projectList.slice((pageNum - 1) * 5, pageNum * 5)],
+        // nextPage: pageNum + 1,
+        isLastPage: pageNum < 5 ? false : true,
       }),
     );
   }),
@@ -24,6 +35,168 @@ function waitLikeServer(time: number) {
 }
 
 const projectList = [
+  {
+    id: '1',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '2',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '3',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '4',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '5',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+
+  {
+    id: '6',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '7',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '8',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '9',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '10',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '1',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '2',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '3',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '4',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '5',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+
+  {
+    id: '6',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '7',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '8',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '9',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
+  {
+    id: '10',
+    projectTitle: 'SLOW-LETTER',
+    projectDescription: '물고기 분류 서비스 입니다.',
+    projectLink: 'https://github.com/SLOW-LETTER/docker-repo',
+    demoSiteLink: '',
+    hashtagArr: ['React', 'TypeScript'],
+  },
   {
     id: '1',
     projectTitle: 'SLOW-LETTER',
