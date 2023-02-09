2e0ae02a0296ae6e557a5aa4132f9e091cc39b66
pione3r <dev2diver@gmail.com>
2023-02-09 17:49:32 +0900

    feat: 로딩 모션 컴포넌트 추가

---
+++ src/components/LoadingDots/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+export function LoadingDots() {
+  return (
+    <LoadingDotsWrapper>
+      <DotLeft />
+      <DotMid />
+      <DotRight />
+    </LoadingDotsWrapper>
+  );
+}
+
+const LoadingDotsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  margin-top: 100px;
+`;
+
+const bounce = keyframes`
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-12px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+`;
+
+const Dot = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  animation: ${bounce} 1s infinite;
+`;
+
+const DotLeft = styled(Dot)`
+  background: #83b4f9;
+  animation-delay: 0.1s;
+`;
+
+const DotMid = styled(Dot)`
+  background: #4880ee;
+  animation-delay: 0.2s;
+`;
+
+const DotRight = styled(Dot)`
+  background: #1f4ef5;
+  animation-delay: 0.3s;
+`;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export * from './ProjectUploadModal';
 export * from './ProjectCardList';
 export * from './Header';
+export * from './LoadingDots';
