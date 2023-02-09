cef25b0c6ceebdc4372585a9e98ec7ae877db3b5
pione3r <dev2diver@gmail.com>
2023-02-09 16:23:44 +0900

    feat: 메인 페이지 / 404 페이지 라우트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,23 @@
 import React, { useEffect } from 'react';
 
-import { ProjectCardList, ProjectUploadModal } from '@/components';
+import { Header, ProjectUploadModal } from '@/components';
 
-import { useDispatch } from 'react-redux';
-import { modalOpen } from '@/RTK/slices/modalSlice';
+import { Route, Routes } from 'react-router-dom';
+import { MainPage } from '@/pages';
 
 export default function App() {
-  const dispatch = useDispatch();
-
   useEffect(() => {
     const colorMode = window.localStorage.getItem('color_mode') || 'dark';
     window.document.body.classList.add(colorMode);
   }, []);
 
   return (
-    <div id="App" className="App" style={{ height: 3000 }}>
-      <button onClick={() => dispatch(modalOpen())} style={{ position: 'relative', zIndex: 1000 }}>
-        모달 열기
-      </button>
-      <button
-        onClick={() => {
-          const colorMode = window.document.body.classList.item(0);
-          if (colorMode === 'dark') {
-            window.document.body.classList.replace('dark', 'light');
-            window.localStorage.setItem('color_mode', 'light');
-          }
-          if (colorMode === 'light') {
-            window.document.body.classList.replace('light', 'dark');
-            window.localStorage.setItem('color_mode', 'dark');
-          }
-        }}
-        style={{ position: 'relative', zIndex: 1000 }}
-      >
-        다크 모드 활성화
-      </button>
-
-      <ProjectCardList />
+    <div id="App">
+      <Header />
+      <Routes>
+        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
 
       <ProjectUploadModal />
     </div>
