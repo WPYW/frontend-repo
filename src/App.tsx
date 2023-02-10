76eea5f63b5db970cf31e988b5ee43a7c3d7ecbf
pione3r <dev2diver@gmail.com>
2023-02-11 01:43:33 +0900

    feat: 헤더 컴포넌트
    - 토글 버튼 스타일 변경
    - 모달 버튼 제거 => 메인 페이지 Description 컴포넌트로 이동 예정
    - 라이트 / 다크모드 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { Header, ProjectDetail } from '@/components';
 
@@ -6,11 +6,6 @@ import { Route, Routes } from 'react-router-dom';
 import { MainPage } from '@/pages';
 
 export default function App() {
-  useEffect(() => {
-    const colorMode = window.localStorage.getItem('color_mode') || 'dark';
-    window.document.body.classList.add(colorMode);
-  }, []);
-
   return (
     <div id="App">
       <Header />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,70 +1,95 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styled, { css, keyframes } from 'styled-components';
 
 import { useDispatch } from 'react-redux';
 import { modalOpen } from '@/RTK/slices/modalSlice';
 
 import { ProjectUploadModal } from '@/components';
 
+import { ReactComponent as HeaderLogoLight } from '@/assets/logo-light.svg';
+import { ReactComponent as HeaderLogodark } from '@/assets/logo-dark.svg';
+
+import { ReactComponent as LightSun } from '@/assets/light-mode-sun.svg';
+import { ReactComponent as LightMoon } from '@/assets/light-mode-moon.svg';
+
+import { ReactComponent as DarkSun } from '@/assets/dark-mode-sun.svg';
+import { ReactComponent as DarkMoon } from '@/assets/dark-mode-moon.svg';
+
 export function Header() {
   const dispatch = useDispatch();
+
+  const [mode, setMode] = useState('');
+
+  useEffect(() => {
+    const colorMode = window.localStorage.getItem('color_mode') || 'dark';
+    window.document.body.classList.add(colorMode);
+    setMode(window.document.body.classList.item(0) || 'dark');
+  }, []);
 
   const DarkModeHandler = () => {
     const colorMode = window.document.body.classList.item(0);
     if (colorMode === 'dark') {
       window.document.body.classList.replace('dark', 'light');
       window.localStorage.setItem('color_mode', 'light');
+      setMode(window.document.body.classList.item(0) || 'light');
     }
     if (colorMode === 'light') {
       window.document.body.classList.replace('light', 'dark');
       window.localStorage.setItem('color_mode', 'dark');
+      setMode(window.document.body.classList.item(0) || 'dark');
     }
   };
 
   return (
     <HeaderWrapper>
-      <HeaderLogoWrapper>
-        <HeaderLogo>LOGO</HeaderLogo>
-      </HeaderLogoWrapper>
-      <ButtonWrapper>
-        <ProjectUploadModalButton onClick={() => dispatch(modalOpen())}>
+      <Link to="/">
+        <HeaderLogoWrapper>
+          {mode === 'light' && <HeaderLogoLight />}
+          {mode === 'dark' && <HeaderLogodark />}
+        </HeaderLogoWrapper>
+      </Link>
+      {/* <ProjectUploadModalButton onClick={() => dispatch(modalOpen())}>
           링크 등록
-        </ProjectUploadModalButton>
-        <DarkModeButton onClick={DarkModeHandler}>다크 모드</DarkModeButton>
-      </ButtonWrapper>
+        </ProjectUploadModalButton> */}
+      <DarkModeToggleWrapper onClick={DarkModeHandler} data-mode={mode}>
+        <DarkModeToggleSunWrapper>
+          {mode === 'light' && (
+            <LightSun style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          )}
+          {mode === 'dark' && (
+            <DarkSun style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          )}
+        </DarkModeToggleSunWrapper>
+        <DarkModeToggleMoonWrapper>
+          {mode === 'light' && (
+            <LightMoon style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          )}{' '}
+          {mode === 'dark' && (
+            <DarkMoon style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          )}
+        </DarkModeToggleMoonWrapper>
+      </DarkModeToggleWrapper>
       <ProjectUploadModal />
     </HeaderWrapper>
   );
 }
 
 const HeaderWrapper = styled.header`
-  border: 2px solid limegreen;
-
   display: flex;
   align-items: center;
   justify-content: space-between;
 
   position: sticky;
   top: 0;
+
+  padding: 16px 32px 16px 32px;
+
+  background-color: var(--header-background-color);
 `;
 
 const HeaderLogoWrapper = styled.div`
-  border: 2px solid limegreen;
-
-  padding: 16px;
-`;
-
-const HeaderLogo = styled.span`
-  border: 2px solid limegreen;
-`;
-
-const ButtonWrapper = styled.div`
-  border: 2px solid limegreen;
-
-  padding: 16px;
-
-  display: flex;
-  gap: 20px;
+  padding: 8px;
 `;
 
 const ProjectUploadModalButton = styled.button`
@@ -73,8 +98,78 @@ const ProjectUploadModalButton = styled.button`
   background: none;
 `;
 
-const DarkModeButton = styled.button`
-  border: 2px solid limegreen;
+const LightToDark = keyframes`
+  0% {
+    transform: translateX(50px);
+  }
 
-  background: none;
+  100% {
+    transform: translateX(0px);
+  }
+`;
+
+const DarkToLight = keyframes`
+  0% {
+    transform: translateX(-50px);
+  }
+
+  100% {
+    transform: translateX(0px);
+  }
+`;
+
+const DarkModeToggleWrapper = styled.div<{ 'data-mode': string }>`
+  border: 2px solid;
+  border-color: var(--dark-mode-toggle-border-color);
+  border-radius: 100px;
+
+  padding: 8px 12px;
+
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  position: relative;
+
+  cursor: pointer;
+
+  &::after {
+    content: '';
+
+    display: block;
+    width: 30px;
+    height: 30px;
+
+    background-color: var(--dark-mode-toggle-background-color);
+    border-radius: 50%;
+
+    position: absolute;
+
+    ${(props) =>
+      props['data-mode'] === 'light'
+        ? css`
+            left: 7px;
+            animation: ${LightToDark} 0.5s;
+          `
+        : css`
+            right: 7px;
+            animation: ${DarkToLight} 0.5s;
+          `}
+
+    z-index: -1;
+  }
+`;
+
+const DarkModeToggleSunWrapper = styled.div`
+  width: 20px;
+  height: 20px;
+
+  object-fit: cover;
+`;
+
+const DarkModeToggleMoonWrapper = styled.div`
+  width: 20px;
+  height: 20px;
+
+  object-fit: cover;
 `;
