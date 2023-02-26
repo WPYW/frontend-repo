f50746b4fb6a7eeab8122eb33c80850f5222d5e5
pione3r <dev2diver@gmail.com>
2023-02-26 18:33:21 +0900

    type: BaseButton 컴포넌트 onClick 타입 추가

--- src/components/atoms/BaseButton/index.types.ts
+++ src/components/atoms/BaseButton/index.types.ts
@@ -2,4 +2,5 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 export interface BaseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  onClick?: () => void;
 }
