854781ba283df009349e77624391972164f47d92
pione3r <dev2diver@gmail.com>
2023-02-26 18:32:20 +0900

    feat: MainPageDescription Block 컴포넌트 구현
    - 컴포넌트
    - 스타일
    - 스토리북

--- src/components/blocks/PageDescription/index.tsx
+++ src/components/blocks/PageDescription/index.tsx
@@ -4,6 +4,7 @@ import * as S from './index.styles';
 import { BaseTitle, BaseDescription } from '@/components/atoms';
 import { PageDescriptionProps } from './index.types';
 
+/** white-space 적용하려면 description={\`설명\`} 같은 방식으로 사용 */
 export function PageDescription({ title, description }: PageDescriptionProps) {
   return (
     <S.Wrapper>
