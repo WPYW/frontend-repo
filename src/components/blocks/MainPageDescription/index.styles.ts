07d33ab4813d1af9182be571a61f431d6443e6a6
pione3r <dev2diver@gmail.com>
2023-03-01 22:43:47 +0900

    style: MainPageDescription 스타일 수정

--- src/components/blocks/MainPageDescription/index.styles.ts
+++ src/components/blocks/MainPageDescription/index.styles.ts
@@ -2,17 +2,11 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
-
-  @media screen and (max-width: 420px) {
-    flex-direction: column;
-  }
-`;
-
-export const SubWrapper = styled.div`
-  display: flex;
   flex-direction: column;
   align-items: center;
   gap: 20px;
+
+  width: fit-content;
+
+  margin-top: 30px;
 `;
