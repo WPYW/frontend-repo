0f64727043eea9c560720bbc5ec2e373750a06d1
pione3r <dev2diver@gmail.com>
2023-03-12 15:09:32 +0900

    chore: storybook msw addon 설치 및 init

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,9 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 import { Provider } from 'react-redux';
-import { store } from '../src/RTK/stores/store';
+import { store } from '../src/stores/store';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+initialize();
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -33,4 +37,5 @@ export const decorators = [
       </QueryClientProvider>
     </Provider>
   ),
+  mswDecorator,
 ];
