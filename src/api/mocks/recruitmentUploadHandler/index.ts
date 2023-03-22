b02cce61f0a6a308fa2a6cd8f6ab82afca8409f5
wooyeonwon <dndus567@naver.com>
2023-03-22 20:06:15 +0900

    [refactor] 모집 게시판 핸들러 코드 리팩토링

--- src/api/mocks/recruitmentUploadHandler/index.ts
+++ src/api/mocks/recruitmentUploadHandler/index.ts
@@ -3,7 +3,7 @@ import { rest } from 'msw';
 // import { BACKEND_API_URL } from '@/common/url';
 
 export const recruitmentUploadHandlers = [
-  rest.post(`/recurit`, async (req, res, ctx) => {
+  rest.post(`/recruit`, async (req, res, ctx) => {
     return res(ctx.status(201), ctx.delay(4000), ctx.json(req.body));
   }),
 ];
