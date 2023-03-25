4e54a0edfdbc8d5dcf499f116f8227f94dc3b92a
wooyeonwon <dndus567@naver.com>
2023-03-25 12:00:59 +0900

    [refactor] 스터디,모집 핸들러 코드 리팩토링

---
+++ src/api/mocks/recruitProject/index.ts
@@ -0,0 +1,51 @@
+import { rest } from 'msw';
+import { BACKEND_API_URL } from '@/common/url';
+import { recruitDetails, recruits } from './dummyData';
+import { RecruitmentListHeader } from '@/components/blocks';
+
+const recruitmentUploadHandlers = rest.post(`/recruits`, async (req, res, ctx) => {
+  return res(ctx.status(201), ctx.delay(4000), ctx.json(req.body));
+});
+
+const recuritmentListHandlers = rest.get(`${BACKEND_API_URL}/recruits`, async (req, res, ctx) => {
+  return res(ctx.status(200), ctx.delay(4000), ctx.json(recruits[0]));
+});
+
+const recruitmentDetailHandlers = rest.get(`/recruits/:id`, async (req, res, ctx) => {
+  let recruitDetail;
+  for (const recruit of recruitDetails) {
+    if (recruit.id === req.url.pathname.replace('/api/v1/recruits/', '')) recruitDetail = recruit;
+  }
+  return res(ctx.status(200), ctx.json({ ...recruitDetail }));
+});
+
+interface PostAddCommentReqBody {
+  content: string;
+}
+
+const addCommentHandler = rest.post<PostAddCommentReqBody>(
+  `${BACKEND_API_URL}/recruits/:id/comments`,
+  async (req, res, ctx) => {
+    let comments = [];
+    for (const recruit of recruitDetails) {
+      if (
+        recruit.id === req.url.pathname.replace('/api/v1/recruits/', '').replace('/comments/', '')
+      )
+        comments = recruit.comments;
+    }
+
+    comments.unshift({
+      nickName: '물에 빠진 너구리',
+      created: '2023-02-23',
+      content: req.body.content,
+    });
+    return res(ctx.status(201), ctx.delay(1000));
+  },
+);
+
+export const recruitProjectHandlers = [
+  recruitmentDetailHandlers,
+  recruitmentUploadHandlers,
+  recuritmentListHandlers,
+  addCommentHandler,
+];
