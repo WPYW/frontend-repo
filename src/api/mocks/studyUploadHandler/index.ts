46384d3029b3f8a2a5d9ac8ce183f92bb99ea46c
pione3r <dev2diver@gmail.com>
2023-03-25 13:33:28 +0900

    chore: 모집 mock api 핸들러 수정

--- src/api/mocks/studyUploadHandler/index.ts
+++
@@ -1,9 +0,0 @@
-import { rest } from 'msw';
-
-// import { BACKEND_API_URL } from '@/common/url';
-
-export const studyUploadHandlers = [
-  rest.post(`/study`, async (req, res, ctx) => {
-    return res(ctx.status(201), ctx.delay(4000), ctx.json(req.body));
-  }),
-];
