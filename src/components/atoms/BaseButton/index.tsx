0c48a256c5bb3e3737c0a40193296d228116039f
pione3r <dev2diver@gmail.com>
2023-04-04 18:10:09 +0900

    chore: 미사용 컴포넌트 삭제

--- src/components/atoms/BaseButton/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Wrapper } from './index.styles';
-import { BaseButtonProps } from './index.types';
-
-export function BaseButton({ children, ...rest }: BaseButtonProps) {
-  return <Wrapper {...rest}>{children}</Wrapper>;
-}
